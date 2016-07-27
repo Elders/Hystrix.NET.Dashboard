daa5d0ea7b06eab0e923447c542ce3c35da80802
mynkow <mynkow@gmail.com>
2016-07-27 11:06:42 +0300

    Moved from Hystrix.NET repo

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,2 @@
+#### 1.0.0 - 27.07.2016
+* Hystrix dashboard as OWIN middleware
---
+++ src/Elders.Hystrix.NET.Dashboard/OwinConfig.cs
@@ -0,0 +1,38 @@
+﻿using Elders.Hystrix.NET.Dashboard.Logging;
+using Elders.Hystrix.NET.Dashboard.StaticFiles;
+using Microsoft.Owin;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using Owin;
+
+namespace Elders.Hystrix.NET.Dashboard
+{
+    public class OwinConfig
+    {
+        private HystrixProxyStream proxyStream;
+
+        private static readonly ILog Logger = LogProvider.GetLogger(typeof(OwinConfig));
+
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var staticFilesNamespace = typeof(StaticFilesNameSpace).Namespace;
+            var assembly = typeof(OwinConfig).Assembly.GetManifestResourceNames();
+
+            var options = new FileServerOptions
+            {
+                EnableDirectoryBrowsing = true,
+                RequestPath = new PathString("/dashboard"),
+                FileSystem = new EmbeddedResourceFileSystem(typeof(OwinConfig).Assembly, staticFilesNamespace)
+            };
+            proxyStream = new HystrixProxyStream();
+            options.StaticFileOptions.ServeUnknownFileTypes = true;
+            options.DefaultFilesOptions.DefaultFileNames = new[] { "index.html" };
+            appBuilder.Use((context, next) =>
+            {
+                proxyStream.ProcessRequest(context);
+                return next.Invoke();
+            });
+            appBuilder.UseFileServer(options);
+        }
+    }
+}
