cd5d0197efc4885a3e7de60c322886796d9fa0a1
mynkow <mynkow@gmail.com>
2016-09-01 14:13:43 +0300

    Logs proper error messages when dashboard is unable to start listener on the provided host

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,5 @@
+#### 1.0.1 - 01.09.2016
+* Logs proper error messages when dashboard is unable to start listener on the provided host
+
 #### 1.0.0 - 27.07.2016
 * Hystrix dashboard as OWIN middleware
--- src/Elders.Hystrix.NET.Dashboard/HystrixDashboard.cs
+++ src/Elders.Hystrix.NET.Dashboard/HystrixDashboard.cs
@@ -1,23 +1,37 @@
 ﻿using Elders.Hystrix.NET.Dashboard.Logging;
 using Microsoft.Owin.Hosting;
 using System;
+using System.Reflection;
 
 namespace Elders.Hystrix.NET.Dashboard
 {
     public static class HystrixDashboard
     {
-        private static readonly ILog Logger = LogProvider.GetLogger(typeof(OwinConfig));
+        static readonly ILog log = LogProvider.GetLogger(typeof(OwinConfig));
 
         static IDisposable app;
 
         public static void Selfhost(string address)
         {
-            if (app != null)
-                throw new InvalidOperationException("Hysrix Dashboard is already hosted. HystrixDashboard.Selfhost can be called only once");
-            Logger.Info($"Started dashboard on {address}");
-            var listener = typeof(Microsoft.Owin.Host.HttpListener.OwinHttpListener);
-            app = WebApp.Start<OwinConfig>(url: address);
-
+            try
+            {
+                if (app != null)
+                    throw new InvalidOperationException("Hysrix Dashboard is already hosted. HystrixDashboard.Selfhost can be called only once");
+                log.Info($"Started dashboard on {address}");
+                var listener = typeof(Microsoft.Owin.Host.HttpListener.OwinHttpListener);
+                app = WebApp.Start<OwinConfig>(url: address);
+            }
+            catch (TargetInvocationException ex)
+            {
+                string command = $"netsh http add urlacl url = {address} user=Everyone listen=yes";
+                string exMessage = "Unable to start listener. Try: " + command;
+                log.FatalException(exMessage, ex);
+                throw new TargetInvocationException(exMessage, ex);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
         public static void Stop()
         {
