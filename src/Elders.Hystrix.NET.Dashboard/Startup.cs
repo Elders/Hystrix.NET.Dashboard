440a208a3253e19706bda383fce4757e152d1289
sdluxeon <sd.luxeon@gmail.com>
2018-05-17 17:33:41 +0300

    Fixes the Ui A Bit

---
+++ src/Elders.Hystrix.NET.Dashboard/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elders.Hystrix.NET.Dashboard
+{
+    public static class ConsoleApplication
+    {
+        public static void Main(string[] args)
+        {
+            var port = 9000;
+            if (args != null && args.Length > 0)
+            {
+                port = int.Parse(args[0]);
+            }
+            HystrixDashboard.Selfhost($"http://+:{port}/");
+            Console.WriteLine($"Dashboard started on http://127.0.0.1:{port}/dashboard/");
+            Console.WriteLine($"Press enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
